abea1a35b6f91f55805c11ba2e4fd2f63d363305
talgat <15talgat@mail.ru>
2022-12-04 18:39:56 +0300

    done zadacha4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+
+int[] RandomDigit(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+int EntryNumber(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int i;
+    for (i = 0; i < count; i++)
+    {
+        Console.Write(col[i] + " ");
+    }
+}
+int size = EntryNumber("введите число: ");
+int[] massiv = new int[size];
+RandomDigit(massiv);
+PrintArray(massiv);
