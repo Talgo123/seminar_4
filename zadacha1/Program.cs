13f969dc24ab6e46e4456c32e877fef896bd7e8d
talgat <15talgat@mail.ru>
2022-12-04 16:16:56 +0300

    done zadacha1

--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+
+int EntryNumber(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// пишем цикл от 1 до А
+// суммируем итератор
+
+int FindNumber(int number)
+{
+    int summ = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        summ = summ + i;
+    }
+    return summ;
+}
+
+int number = EntryNumber("Введите число:");
+int result = FindNumber(number);
+Console.WriteLine($"{number} -> {result} ");
